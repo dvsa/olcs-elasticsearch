97b0b9d139860b22d40930bfba817a1d08038273
Gary.Jones <garyjones@gj-macbook-2.local>
2015-03-31 11:26:47 +0100

    First version of addresses index

--- elastic_scripts/oc_corr_address/createOCAddressIndex.sh
+++ elastic_scripts/oc_corr_address/createOCAddressIndex.sh
@@ -1,62 +1,90 @@
-curl -XPUT 'localhost:9200/application_v1' -d '
+curl -XPUT 'localhost:9200/address_v1' -d '
 {
   "aliases" : {
-      "application" : {}
+      "address" : {}
   },
   "mappings": {
-    "application": {
+    "address": {
       "_all": {
         "type": "string",
         "null_value": "na",
         "index": "analyzed",
-        "analyzer": "application_ngram_analyzer"
+        "analyzer": "address_ngram_analyzer"
       },
       "properties" : {
-          "app_id" : {
-            "type" : "long"
-          },
-          "app_status_desc" : {
-            "type" : "string"
-          },
-          "correspondence_postcode" : {
-            "type" : "string",
-            "analyzer" : "application_edgengram_analyzer"
-          },
-          "lic_id" : {
+    	"addr_id" : {
             "type" : "long",
             "include_in_all" : false
           },
-          "lic_no" : {
+          "app_id" : {
+            "type" : "long",
+            "include_in_all" : false
+          },
+        "org_id" : {
+            "type" : "long",
+            "include_in_all" : false
+          },
+        "oc_id" : {
+            "type" : "long",
+            "include_in_all" : false
+          },
+        "aoc_id" : {
+            "type" : "long",
+            "include_in_all" : false
+          },
+        "lic_id" : {
+            "type" : "long",
+            "include_in_all" : false
+          },
+        "lic_no" : {
             "type" : "string",
-            "analyzer" : "application_ngram_analyzer"
+            "analyzer" : "address_ngram_analyzer"
           },
-          "lic_status_desc" : {
+        "paon_desc" : {
+            "type" : "string",
+            "analyzer" : "address_ngram_analyzer"
+          },
+        "saon_desc" : {
+            "type" : "string",
+            "analyzer" : "address_ngram_analyzer"
+          },
+        "street" : {
+            "type" : "string",
+            "analyzer" : "address_ngram_analyzer"
+          },
+        "locality" : {
+            "type" : "string",
+            "analyzer" : "address_ngram_analyzer"
+          },
+        "town" : {
+            "type" : "string",
+            "analyzer" : "address_ngram_analyzer"
+          },
+        "postcode" : {
+            "type" : "string",
+            "analyzer" : "address_edgengram_analyzer"
+          },
+          "country_code" : {
             "type" : "string"
           },
-          "lic_type_desc" : {
+		"app_status" : {
             "type" : "string"
           },
-          "org_name" : {
+		"lic_status" : {
+            "type" : "string"
+          },
+          "lic_type" : {
+            "type" : "string"
+          },
+        "org_name" : {
             "type" : "string",
             "index" : "not_analyzed"
-          },
-          "org_id" : {
-            "type" : "long",
-            "include_in_all" : false
           },
           "org_name_wildcard" : {
             "type" : "string",
             "index" : "not_analyzed"
           },
-          "received_date" : {
-            "type" : "date",
-            "format" : "dateOptionalTime"
-          },
-          "tot_auth_trailers" : {
-            "type" : "long",
-            "include_in_all" : false
-          },
-          "tot_auth_vehicles" : {
+        "oc_opposition_count" : {
             "type" : "long",
             "include_in_all" : false
           }
@@ -73,18 +101,18 @@ curl -XPUT 'localhost:9200/application_v1' -d '
         } 
       },
       "analyzer": {
-        "application_ngram_analyzer": {
-          "tokenizer": "application_ngram_tokenizer",
+        "address_ngram_analyzer": {
+          "tokenizer": "address_ngram_tokenizer",
           "filter" : ["standard", "lowercase", "stop"]
         },
-        "application_edgengram_analyzer": {
-          "tokenizer": "application_edgengram_tokenizer",
+        "address_edgengram_analyzer": {
+          "tokenizer": "address_edgengram_tokenizer",
           "filter" : ["standard", "lowercase", "stop"],
           "char_filter" : ["spaces_removed_pattern"]
         }
       },
       "tokenizer": {
-        "application_ngram_tokenizer": {
+        "address_ngram_tokenizer": {
           "type": "nGram",
           "min_gram": "4",
           "max_gram": "10",
@@ -93,7 +121,7 @@ curl -XPUT 'localhost:9200/application_v1' -d '
             "digit"
           ]
         },
-        "application_edgengram_tokenizer": {
+        "address_edgengram_tokenizer": {
           "type": "edgeNGram",
           "min_gram": "2",
           "max_gram": "10",
--- elastic_scripts/oc_corr_address/createOCAddressRiverUpdates.sh
+++ elastic_scripts/oc_corr_address/createOCAddressRiverUpdates.sh
@@ -1,17 +1,17 @@
-db=$0
-username=$1
-password=$2
+db=$1
+username=$2
+password=$3
 
-curl -XPUT 'localhost:9200/_river/olcs_application_river/_meta' -d '{ 
+curl -XPUT 'localhost:9200/_river/olcs_address_river/_meta' -d '{ 
     "type": "jdbc", 
     "jdbc": {       
         "driver": "com.mysql.jdbc.Driver",  
-        "url": "jdbc:mysql://localhost:3306/${db}",
-        "user": "${username}",
-        "password": "${password}",
-        "schedule" : "0 2/10 0-23 ? * *",
-        "sql": [{"statement":"update elastic_updates set previous_runtime=runtime, runtime=unix_timestamp(now()) where index_name = \"application\""},{"statement" : "select concat_ws(\"_\", ifnull(o.id, \"none\"), ifnull(l.id, \"none\"), ifnull(a.id, \"none\"), ifnull(ad.id, \"none\")) as _id, a.id app_id, l.id lic_id, l.lic_no, o.id org_id, o.name org_name, lower(o.name) org_name_wildcard, ad.postcode correspondence_postcode, a.tot_auth_vehicles, a.tot_auth_trailers, a.received_date, rd_lt.description lic_type_desc, rd_ls.description lic_status_desc, rd_as.description app_status_desc from application a inner join licence l on (a.licence_id = l.id) inner join organisation o on (l.organisation_id = o.id) left join (contact_details cd, address ad) on (cd.licence_id = l.id and cd.Contact_Type = \"ct_corr\" and cd.address_id = ad.id) inner join ref_data rd_lt on (rd_lt.id = l.licence_type) inner join ref_data rd_ls on (rd_ls.id = l.status) inner join ref_data rd_as on (rd_as.id = a.status) inner join elastic_updates eu ON (eu.index_name = \"application\") where (ad.last_modified_on > from_unixtime(eu.previous_runtime) or a.last_modified_on > from_unixtime(eu.previous_runtime) or l.last_modified_on > from_unixtime(eu.previous_runtime) or o.last_modified_on > from_unixtime(eu.previous_runtime))"}],
-        "index": "application_v1",
-        "type": "application"
+        "url": "jdbc:mysql://localhost:3306/'"$db"'",
+        "user": "'"$username"'", 
+        "password": "'"$password"'",
+        "schedule" : "0 3/10 0-23 ? * *",
+        "sql": [{"statement":"update elastic_updates set previous_runtime=runtime, runtime=unix_timestamp(now()) where index_name = \"addresses\""},{"statement" : "SELECT concat_ws(\"_\",ifnull(addr.id,\"none\"),ifnull(app.id,\"none\"),ifnull(org.id,\"none\"),ifnull(oc.id,\"none\"),ifnull(aoc.id,\"none\"),ifnull(lic.id,\"none\")) as _id, addr.id addr_id, app.id as app_id, org.id as org_id, oc.id as oc_id, aoc.id as aoc_id, lic.id as lic_id, lic.lic_no as lic_no, addr.paon_desc as paon_desc, addr.saon_desc as saon_desc, addr.street as street, addr.locality as locality, addr.town as town, addr.postcode as postcode, addr.country_code as cc, (select description from ref_data where id = app.status) as app_status, (select description from ref_data where id = lic.status) as lic_status, (select description from ref_data where id = lic.licence_type) as lic_type, lic.lic_no as lic_no, org.name org_name, LOWER(org.name) org_name_wildcard,, (SELECT count( oco.operating_centre_id) as count FROM operating_centre_opposition oco WHERE (oco.operating_centre_id = oc.id)) AS oc_opposition_count FROM address addr INNER JOIN operating_centre oc ON addr.id = oc.address_id INNER JOIN application_operating_centre aoc ON oc.id = aoc.operating_centre_id INNER JOIN application app ON aoc.application_id = app.id INNER JOIN licence lic ON app.licence_id = lic.id INNER JOIN organisation org ON lic.organisation_id = org.id inner join elastic_updates eu ON (eu.index_name = \"addresses\") where (addr.last_modified_on > from_unixtime(eu.previous_runtime) or app.last_modified_on > from_unixtime(eu.previous_runtime) or org.last_modified_on > from_unixtime(eu.previous_runtime) or oc.last_modified_on > from_unixtime(eu.previous_runtime) or aoc.last_modified_on > from_unixtime(eu.previous_runtime) or lic.last_modified_on > from_unixtime(eu.previous_runtime))"}],
+        "index": "address_v1",
+        "type": "address"
     }  
 }'
