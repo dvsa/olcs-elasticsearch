60418f66185e88d742ec9855415c29f2cdb8055d
Gary Jones <jonesg@scdv-db02.sc.npm>
2015-04-02 14:10:16 +0100

    Added params to script

--- elastic_scripts/user/createUserRiverUpdates.sh
+++ elastic_scripts/user/createUserRiverUpdates.sh
@@ -1,14 +1,14 @@
-db=$0
-username=$1
-password=$2
+db=$1
+username=$2
+password=$3
 
 curl -XPUT 'localhost:9200/_river/olcs_user_river/_meta' -d '{ 
     "type": "jdbc", 
     "jdbc": {       
         "driver": "com.mysql.jdbc.Driver",  
-        "url": "jdbc:mysql://localhost:3306/olcsdev",
-        "user": "olcsdev",
-        "password": "password",
+        "url": "jdbc:mysql://localhost:3306/'"$db"'",
+        "user": "'"$username"'", 
+        "password": "'"$password"'",
         "schedule" : "0 2/10 0-23 ? * *",
         "sql": [{"statement":"update elastic_updates set previous_runtime=runtime, runtime=unix_timestamp(now()) where index_name = \"user\""},{"statement" : "select concat_ws(\"_\", ifnull(u.id, \"none\"), ifnull(r.id, \"none\"), ifnull(o.id, \"none\"), ifnull(cd.id, \"none\")) as _id, u.id user_id, r.id role_id, o.id org_id, cd.id con_det_id, u.pid identity_pid, u.last_successful_login_date, u.team_id, cd.email_address, cd.forename, cd.family_name, t.name team_name, o.name org_name, case when (r.code like \"CW%\" or r.code = \"ADMIN\" or r.code = \"SYS\") then \"Internal\" when (r.code = \"LA\") then \"Local Authority\" when (r.code like \"PART%\") then \"Partner\" when (r.code = \"TM\") then \"Transport Manager\" else \"Operator\" end user_type, r.role, case when (u.locked_date is not null) then \"Yes\" else \"No\" end disabled, t.name description from team t inner join user u on (u.team_id=t.id) inner join user_role ur on (ur.user_id=u.id) inner join role r on (r.id=ur.role_id) left join organisation_user ou on (ou.user_id = u.id) left join organisation o on (o.id = ou.organisation_id) inner join contact_details cd on (cd.id = u.contact_details_id) inner join elastic_updates eu ON (eu.index_name = \"user\") where (u.last_modified_on > from_unixtime(eu.previous_runtime) or r.last_modified_on > from_unixtime(eu.previous_runtime) or o.last_modified_on > from_unixtime(eu.previous_runtime) or cd.last_modified_on > from_unixtime(eu.previous_runtime))"}],
         "index": "user_v1",
