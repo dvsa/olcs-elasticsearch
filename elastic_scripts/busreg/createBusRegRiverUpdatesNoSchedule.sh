b072f7a202a6d67ab68a765b27580c2f00bfb5c6
Gary Jones <jonesg@elasticsearch-dev.olcs.mgt.mtpdvsa>
2015-05-29 09:33:23 +0000

    Modified to run against a remote database

--- elastic_scripts/busreg/createBusRegRiverUpdatesNoSchedule.sh
+++ elastic_scripts/busreg/createBusRegRiverUpdatesNoSchedule.sh
@@ -19,7 +19,6 @@ curl -XPUT 'localhost:9200/_river/olcs_busreg_river/_meta' -d '{
         "url": "jdbc:mysql://'"$host"':3306/'"$db"'",
         "user": "'"$username"'", 
         "password": "'"$password"'",
-        "schedule" : "0 2/10 0-23 ? * *",
         "sql":[{"statement":"update elastic_updates set previous_runtime=runtime, runtime=unix_timestamp(now()) where index_name = \"busreg\""},{"statement":"'"$final_sql"'"}],
         "index": "busreg_v1",
         "type": "busreg"
