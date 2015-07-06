f43c16ca3f966e5b61b7d42a143612700acbfa0e
jonesg <gary.jones@valtech.co.uk>
2015-07-06 15:57:40 +0100

    Initial version of scripts for recipients

---
+++ recipient/createRecipientIndex.sh
@@ -0,0 +1,83 @@
+curl -XPUT 'localhost:9200/recipient_v1' -d '
+{
+  "aliases" : {
+      "recipient" : {}
+  },
+  "mappings": {
+    "recipient": {
+      "_all": {
+        "type": "string",
+        "null_value": "na",
+        "index": "analyzed",
+        "analyzer": "recipient_ngram_analyzer"
+      },
+      "properties" : {
+        "ta_id" : {
+            "type" : "long"
+          },
+        "r_id" : {
+            "type" : "long"
+          },
+        
+        "hearing_date_time" : {
+            "type" : "date",
+            "format" : "dateOptionalTime"
+          },
+        "venue" : {
+            "type" : "string",
+            "analyzer" : "recipient_ngram_analyzer"
+          },
+        "contact_name" : {
+              "type" : "string",
+              "analyzer" : "recipient_ngram_analyzer"
+          },
+        "email_address" : {
+              "type" : "string",
+              "index" : "not_analyzed"
+          }
+    }
+  },
+  "settings": {
+    "analysis": {
+      "char_filter" : {
+        "spaces_removed_pattern":{
+          "type":"pattern_replace",
+          "pattern":"\\s",
+          "replacement":""
+        } 
+      },
+      "analyzer": {
+        "recipient_ngram_analyzer": {
+          "tokenizer": "recipient_ngram_tokenizer",
+          "filter" : ["standard", "lowercase", "stop"]
+        },
+        "recipient_edgengram_analyzer": {
+          "tokenizer": "recipient_edgengram_tokenizer",
+          "filter" : ["standard", "lowercase", "stop"],
+          "char_filter" : ["spaces_removed_pattern"]
+        }
+      },
+      "tokenizer": {
+        "recipient_ngram_tokenizer": {
+          "type": "nGram",
+          "min_gram": "4",
+          "max_gram": "10",
+          "token_chars": [
+            "letter",
+            "digit"
+          ]
+        },
+        "recipient_edgengram_tokenizer": {
+          "type": "edgeNGram",
+          "min_gram": "2",
+          "max_gram": "10",
+          "token_chars": [
+            "letter",
+            "digit"
+          ]
+        }
+      }
+    }
+  }
+}
+'
---
+++ recipient/recipient.sql
@@ -0,0 +1,18 @@
+select
+    CONCAT_WS('_',
+            IFNULL(ta.id, 'none'),
+            IFNULL(rta.recipient_id, 'none')) AS _id,
+    ta.id ta_id,
+    rta.recipient_id r_id, 
+    r.contact_name, 
+    r.email_address
+from
+    recipient r
+        inner join
+    recipient_traffic_area rta ON (rta.recipient_id = r.id)
+        inner join
+    traffic_area ta ON (ta.id = rta.traffic_area_id)
+        INNER JOIN
+    elastic_updates eu ON (eu.index_name = 'recipient')
+WHERE (r.last_modified_on > FROM_UNIXTIME(eu.previous_runtime)
+        OR ta.last_modified_on > FROM_UNIXTIME(eu.previous_runtime))
