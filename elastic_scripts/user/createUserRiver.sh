58539b6a65f369f17e55195964cc42448707b982
jonesg <gary.jones@valtech.co.uk>
2015-10-01 15:32:46 +0100

    Renamed for consistency

--- elastic_scripts/user/createUserRiver.sh
+++ elastic_scripts/user/createUserRiver.sh
@@ -3,7 +3,7 @@ db=$2
 username=$3
 password=$4
 
-sql_script=$(<users.sql)
+sql_script=$(<user.sql)
 escaped_sql=${sql_script//\'/\\\"} 
 final_sql=$(echo $escaped_sql | tr '\n' ' ')
 
@@ -14,7 +14,6 @@ curl -XPUT 'localhost:9200/_river/olcs_user_river/_meta' -d '{
         "url": "jdbc:mysql://'"$host"':3306/'"$db"'",
         "user": "'"$username"'", 
         "password": "'"$password"'", 
-        "schedule" : "0 8/10 0-23 ? * *",
         "sql": [{"statement":"update elastic_update set previous_runtime=runtime, runtime=unix_timestamp(now()) where index_name = \"user\""},{"statement":"'"$final_sql"'"}],
         "index": "user_v1",
         "type": "user"
