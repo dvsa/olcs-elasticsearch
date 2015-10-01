c14ced248088a06164a396a8597803ba3a2a79a8
jonesg <gary.jones@valtech.co.uk>
2015-10-01 15:28:28 +0100

    Renamed for consistency

--- elastic_scripts/publication/createPublicationRiver.sh
+++ elastic_scripts/publication/createPublicationRiver.sh
@@ -3,7 +3,7 @@ db=$2
 username=$3
 password=$4
 
-sql_script=$(<publications.sql)
+sql_script=$(<publication.sql)
 escaped_sql=${sql_script//\'/\\\"} 
 final_sql=$(echo $escaped_sql | tr '\n' ' ')
 
