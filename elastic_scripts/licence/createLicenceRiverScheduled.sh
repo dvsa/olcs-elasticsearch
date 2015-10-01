cfeea7974a8fdbc0bc73cc26bc3839ac7908d2cc
jonesg <gary.jones@valtech.co.uk>
2015-10-01 15:14:14 +0100

    Updated for consistency

--- elastic_scripts/licence/createLicenceRiverScheduled.sh
+++ elastic_scripts/licence/createLicenceRiverScheduled.sh
@@ -3,7 +3,7 @@ db=$2
 username=$3
 password=$4
 
-sql_script=$(<licences.sql)
+sql_script=$(<licence.sql)
 escaped_sql=${sql_script//\'/\\\"} 
 final_sql=$(echo $escaped_sql | tr '\n' ' ')
 
@@ -14,6 +14,7 @@ curl -XPUT 'localhost:9200/_river/olcs_licence_river/_meta' -d '{
         "url": "jdbc:mysql://'"$host"':3306/'"$db"'",
         "user": "'"$username"'", 
         "password": "'"$password"'", 
+        "schedule" : "0 8/10 0-23 ? * *",
         "sql": [{"statement":"update elastic_update set previous_runtime=runtime, runtime=unix_timestamp(now()) where index_name = \"licence\""},{"statement":"'"$final_sql"'"}],
         "index": "licence_v1",
         "type": "licence"
