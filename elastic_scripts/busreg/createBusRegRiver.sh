ea0b7161a90355ade5dd8a1ad0cde2b63374b25b
Gary Jones <jonesg@elasticsearch-dev.olcs.mgt.mtpdvsa>
2015-11-05 14:47:50 +0000

    Added version to rivers

--- elastic_scripts/busreg/createBusRegRiver.sh
+++ elastic_scripts/busreg/createBusRegRiver.sh
@@ -4,13 +4,11 @@ host=$1
 db=$2
 username=$3
 password=$4
-
+version=${5:-1}
 
 sql_script=$(<busreg.sql)
 escaped_sql=${sql_script//\'/\\\"} 
 final_sql=$(echo $escaped_sql | tr '\n' ' ')
-
-echo $value
 
 curl -XPUT 'localhost:9200/_river/olcs_busreg_river/_meta' -d '{ 
     "type": "jdbc", 
@@ -19,8 +17,8 @@ curl -XPUT 'localhost:9200/_river/olcs_busreg_river/_meta' -d '{
         "url": "jdbc:mysql://'"$host"':3306/'"$db"'",
         "user": "'"$username"'", 
         "password": "'"$password"'",
-        "sql":[{"statement":"update elastic_update set previous_runtime=runtime, runtime=unix_timestamp(now()) where index_name = \"bus_reg\""},{"statement":"'"$final_sql"'"}],
-        "index": "busreg_v1",
+        "sql":[{"statement":"update elastic_update set previous_runtime=runtime, runtime=unix_timestamp(now()) where index_name = \"busreg\""},{"statement":"'"$final_sql"'"}],
+        "index": "busreg_v'"$version"'",
         "type": "busreg"
     }  
 }'
--- elastic_scripts/busreg/createBusRegRiverScheduled.sh
+++ elastic_scripts/busreg/createBusRegRiverScheduled.sh
@@ -4,13 +4,12 @@ host=$1
 db=$2
 username=$3
 password=$4
-
+version=${5:-1}
 
 sql_script=$(<busreg.sql)
 escaped_sql=${sql_script//\'/\\\"} 
 final_sql=$(echo $escaped_sql | tr '\n' ' ')
 
-echo $value
 
 curl -XPUT 'localhost:9200/_river/olcs_busreg_river/_meta' -d '{ 
     "type": "jdbc", 
@@ -20,8 +19,8 @@ curl -XPUT 'localhost:9200/_river/olcs_busreg_river/_meta' -d '{
         "user": "'"$username"'", 
         "password": "'"$password"'",
         "schedule" : "0 2/10 0-23 ? * *",
-        "sql":[{"statement":"update elastic_update set previous_runtime=runtime, runtime=unix_timestamp(now()) where index_name = \"bus_reg\""},{"statement":"'"$final_sql"'"}],
-        "index": "busreg_v1",
+        "sql":[{"statement":"update elastic_update set previous_runtime=runtime, runtime=unix_timestamp(now()) where index_name = \"busreg\""},{"statement":"'"$final_sql"'"}],
+        "index": "busreg_v'"$version"'",
         "type": "busreg"
     }  
 }'
